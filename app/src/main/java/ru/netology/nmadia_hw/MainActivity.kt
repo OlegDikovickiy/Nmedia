b25f44b5c1d182c58483eb6f3f69a137b952907b
Oleg Dikovickiy <dikovickiy526@inbox.ru>
2025-08-23 14:47:16 +0300

    Task 7.2 Button cancel edit

--- app/src/main/java/ru/netology/nmadia_hw/MainActivity.kt
+++ app/src/main/java/ru/netology/nmadia_hw/MainActivity.kt
@@ -1,78 +1,77 @@
 package ru.netology.nmadia_hw
 
 import android.os.Bundle
-import android.view.LayoutInflater
+import android.view.View
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import ru.netology.nmadia_hw.adapter.OnInteractionListener
 import ru.netology.nmadia_hw.adapter.PostAdapter
 import ru.netology.nmadia_hw.databinding.ActivityMainBinding
-import ru.netology.nmadia_hw.databinding.CardPostBinding
 import ru.netology.nmadia_hw.dto.Post
 import ru.netology.nmadia_hw.util.AndroidUtils
-
 
 class MainActivity : AppCompatActivity() {
 
     private val viewModel: PostViewModel by viewModels()
-
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val adapter = PostAdapter( object : OnInteractionListener{
-            override fun like(post: Post) {
-                viewModel.like(post.id)
-            }
-
-            override fun share(post: Post) {
-                viewModel.share(post.id)
-            }
-
-            override fun remove(post: Post) {
-                viewModel.remove(post.id)
-            }
-
-            override fun edit(post: Post) {
-                viewModel.edit(post)
-            }
-
+        val adapter = PostAdapter(object : OnInteractionListener {
+            override fun like(post: Post) = viewModel.like(post.id)
+            override fun share(post: Post) = viewModel.share(post.id)
+            override fun remove(post: Post) = viewModel.remove(post.id)
+            override fun edit(post: Post) = viewModel.edit(post)
         })
 
-
-
         binding.list.adapter = adapter
+
         viewModel.data.observe(this) { posts ->
-            val new = posts.size > adapter.currentList.size && adapter.currentList.isNotEmpty()
+            val newPostAdded = posts.size > adapter.currentList.size && adapter.currentList.isNotEmpty()
             adapter.submitList(posts) {
-                if (new) {
-                    binding.list.smoothScrollToPosition(
-                        0
-                    )
+                if (newPostAdded) {
+                    binding.list.smoothScrollToPosition(0)
                 }
             }
         }
-        viewModel.edited.observe(this){
-            if(it.id != 0L){
-                binding.content.setText(it.content)
-                AndroidUtils.showKeyboard(binding.content)
+
+        viewModel.edited.observe(this) { post ->
+            if (post.id != 0L) {
+                binding.content.setText(post.content)
+                binding.originalPreview.text = post.content
+            } else {
+                binding.content.setText("")
+                binding.originalPreview.text = ""
             }
         }
+
+        viewModel.isEditing.observe(this) { editing ->
+            binding.editBlock.visibility = if (editing) View.VISIBLE else View.GONE
+
+            if (editing) {
+                binding.content.requestFocus()
+                AndroidUtils.showKeyboard(binding.content)
+            } else {
+                binding.content.clearFocus()
+                AndroidUtils.hideKeyboard(binding.content)
+            }
+        }
+
         binding.save.setOnClickListener {
             val text = binding.content.text.toString()
             if (text.isBlank()) {
-                Toast.makeText(this@MainActivity, R.string.error_empty_content, Toast.LENGTH_LONG)
-                    .show()
+                Toast.makeText(this, R.string.error_empty_content, Toast.LENGTH_LONG).show()
                 return@setOnClickListener
             }
             viewModel.save(text)
-            binding.content.setText("")
-            binding.content.clearFocus()
-            AndroidUtils.hideKeyboard(binding.content)
+        }
+
+        binding.cancelEdit.setOnClickListener {
+            viewModel.cancelEdit()
         }
     }
 }
-
--- app/src/main/java/ru/netology/nmadia_hw/PostViewModel.kt
+++ app/src/main/java/ru/netology/nmadia_hw/PostViewModel.kt
@@ -3,7 +3,6 @@ package ru.netology.nmadia_hw
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import org.w3c.dom.Text
 import ru.netology.nmadia_hw.dto.Post
 import ru.netology.nmadia_hw.repository.PostRepository
 import ru.netology.nmadia_hw.repository.PostRepositoryInMemoryImpl
@@ -24,9 +23,13 @@ class PostViewModel : ViewModel() {
     val data: LiveData<List<Post>> = repository.getAll()
     val edited = MutableLiveData(empty)
 
+    private val _isEditing = MutableLiveData(false)
+    val isEditing: LiveData<Boolean> = _isEditing
+
     fun like(id: Long) = repository.likeById(id)
     fun share(id: Long) = repository.shareById(id)
     fun remove(id: Long) = repository.removeById(id)
+
     fun save(text: String) {
         edited.value?.let {
             val content = text.trim()
@@ -35,11 +38,16 @@ class PostViewModel : ViewModel() {
             }
         }
         edited.value = empty
+        _isEditing.value = false
     }
 
     fun edit(post: Post) {
         edited.value = post
+        _isEditing.value = true
     }
 
-
+    fun cancelEdit() {
+        edited.value = empty
+        _isEditing.value = false
+    }
 }
